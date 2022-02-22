d7605a4006446e4ebcf4f4dc417cbffc97792bd1
Larryman555 <larryman555@gmail.com>
2022-02-21 17:38:38 -0700

    general and special Menu

--- Main.java
+++ Main.java
@@ -66,19 +66,102 @@ public class Main {
             System.out.println("Failed retrieval, game or year does not exist!");
             return null;
     }
+    public static String findGame(){
+        int choice = 0;
+        String sChoice = "";
+        System.out.println("Teams have their first letter capitalized, no periods for abbreviations");
+        while (sChoice.isEmpty() == true){
+            try{
+                System.out.print("Please Input the Home Team:");
+                sChoice = input.next();
+                if (sChoice.isEmpty() == true){
+                    System.out.println("Invalid Input, Please Try again!");
+                }
+            }
+            catch(InputMismatchException e){
+                System.out.println("Invalid Input! Please Try Again!");
+                input.next();
+            }
+        }
+        String homeTeam = sChoice;
+        sChoice = "";
+        while (sChoice.isEmpty() == true){
+            try{
+                System.out.print("Please Input the Away Team:");
+                sChoice = input.next();
+                if (sChoice.isEmpty() == true){
+                    System.out.println("Invalid Input, Please Try again!");
+                }
+            }
+            catch(InputMismatchException e){
+                System.out.println("Invalid Input! Please Try Again!");
+                input.next();
+            }
+        }
+        String awayTeam = sChoice;
+        sChoice = "";
+        while (choice == 0){
+            try{
+                System.out.print("Please Input the year the game was played:");
+                choice = input.nextInt();
+                if (choice < 1000 || choice > 9999){
+                    choice = 0;
+                    System.out.println("Invalid Input, Please Try again!");
+                }
+            }
+            catch(InputMismatchException e){
+                System.out.println("Invalid Input! Please Try Again!");
+                input.next();
+            }
+        }
+        int gameYear = choice;
+        while (choice == 0){
+            try{
+                System.out.print("Please Input the month the game was played, as a number (1 = January, 2 = February, etc.:");
+                choice = input.nextInt();
+                if (choice < 1 || choice > 12){
+                    choice = 0;
+                    System.out.println("Invalid Input, Please Try again!");
+                }
+            }
+            catch(InputMismatchException e){
+                System.out.println("Invalid Input! Please Try Again!");
+                input.next();
+            }
+        }
+        int gameMonth = choice;
+        while (choice == 0){
+            try{
+                System.out.print("Please Input the day the game was played:");
+                choice = input.nextInt();
+                if (choice < 1 || choice > 31){
+                    choice = 0;
+                    System.out.println("Invalid Input, Please Try again!");
+                }
+            }
+            catch(InputMismatchException e){
+                System.out.println("Invalid Input! Please Try Again!");
+                input.next();
+            }
+        }
+        int gameDay = choice;
+        String Date = gameYear + "-" + gameMonth + "-" + gameDay;
+        String foundGame = homeTeam + " vs. " + awayTeam + " on " + Date;
+        return foundGame;
+    }
     /**
      * @author Robert Engel, T03, Feb 21 2022
      * The main part of the menu system, breaking it up for simplicity of finding specific parts of the menu.
      */
     public static void mainMenu(){
         int choice = 0;
-        System.out.println("Welcome to the Premier League Data Repository Main Menu!\nPlease Input a number to choose your action");
-        System.out.println("1. Add a new Year\n2. Add a new Game to a Year\n3. General Output Menu\n4. Special Output Menu");
+        System.out.println("Welcome to the Premier League Database Main Menu!\nPlease Input a number to choose your action");
+        System.out.println("1. Add a new Year\n2. Add a new Game to a Year\n3. General Output Menu\n4. Special Output Menu\n5. Quit the program");
         while (choice == 0){
             try{
                 System.out.print("Please Input a number:");
                 choice = input.nextInt();
-                if (choice != 1 && choice != 2 && choice != 3 && choice != 4){
+                if (choice < 1 || choice > 5){
                     choice = 0;
                     System.out.println("Invalid Input, Please Try again!");
                 }
@@ -101,8 +184,15 @@ public class Main {
             case 4:
                 specialMenu();
                 break;
+            case 5:
+                System.exit(0);
+                break;
         }
     }
+    /**
+     * @author Robert Engel, T03, Feb 21 2022
+     * the part of the menu where you enter in a new year
+     * */
     public static void yearMenu(){
         System.out.print("Years are formatted as a 4-digit positive number\nPlease Input the new Year you would like to create:");
         int choice = 0;
@@ -123,6 +213,10 @@ public class Main {
         addYear(choice);
         mainMenu();
     }
+    /**
+     * @author Robert Engel, T03, Feb 21 2022
+     * the part of the menu where you enter in a new game, and all its corresponding data
+     * */
     public static void gameMenu(){
         int choice = 0;
         String sChoice = "";
@@ -310,7 +404,91 @@ public class Main {
         int gameDay = choice;
         String Date = gameYear + "-" + gameMonth + "-" + gameDay;
         gameData[9] = Date;
+        addGame(gameYear, gameData);
         mainMenu();
+    }
+    /**
+     * @author Robert Engel, T03, Feb 21 2022
+     * the part of the menu where you ask for general types of input
+     * */
+    public static void generalMenu(){
+        System.out.println("Please Input a Number to choose your action");
+        System.out.println("1.Print the total number of shots in a game\n2.Print the total number of fouls in a game\n3.Print all years in the database\n4.Print all the games in a year\n5.Print all games in the database\n6.Return to the Main Menu");
+        int choice = 0;
+        while (choice == 0){
+            try{
+                System.out.print("Please Input a number:");
+                choice = input.nextInt();
+                if (choice < 1 || choice > 6){
+                    choice = 0;
+                    System.out.println("Invalid Input, Please Try again!");
+                }
+            }
+            catch(InputMismatchException e){
+                System.out.println("Invalid Input! Please Try Again!");
+                input.next();
+            }
+        }
+        switch (choice){
+            case 1:
+                //Total number of shots in a game function
+                break;
+            case 2:
+                //total number of fouls in a game function
+                break;
+            case 3:
+                //print all years function
+                break;
+            case 4:
+                //print all games in a year function
+                break;
+            case 5:
+                //print all games ever function
+                break;
+            case 6:
+                mainMenu();
+                break;
+        }
+    }
+    /**
+     * @author Robert Engel, T03, Feb 21 2022
+     * the part of the menu where you ask for special types of input
+     * */
+    public static void specialMenu(){
+        System.out.println("Please Input a Number to choose your action");
+        System.out.println("1.Print the top 5 games for fouls\n2.Print the top 5 games for goals\n3.Print the top 5 teams for wins\n4. Print the top 5 teams for shots taken\n5.Return to the Main Menu");
+        int choice = 0;
+        while (choice == 0){
+            try{
+                System.out.print("Please Input a number:");
+                choice = input.nextInt();
+                if (choice < 1 || choice > 5){
+                    choice = 0;
+                    System.out.println("Invalid Input, Please Try again!");
+                }
+            }
+            catch(InputMismatchException e){
+                System.out.println("Invalid Input! Please Try Again!");
+                input.next();
+            }
+        }
+        switch (choice){
+            case 1:
+                //Top 5 games for fouls function
+                break;
+            case 2:
+                //top 5 games for goals function
+                break;
+            case 3:
+                //top 5 teams for wins function
+                break;
+            case 4:
+                //top 5 teams for shots taken function
+                break;
+            case 5:
+                mainMenu();
+                break;
+        }
     }
     public static void main(String[] args) {
         mainMenu();
