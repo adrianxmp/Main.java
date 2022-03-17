a625878a301116015b1853710f0967f8bdd30c53
Larryman555 <larryman555@gmail.com>
2022-03-17 14:43:24 -0700

    Timeline except for CSV, some of Year

--- Timeline.java
+++ Timeline.java
@@ -1,15 +1,20 @@
 import java.util.*;
 public class Timeline {
-    private static ArrayList<Year> timeline = new ArrayList<Year>();
+    private static final ArrayList<Year> timeline = new ArrayList<Year>();
     public ArrayList<Integer> getAllYears(){
         ArrayList<Integer> yearList = new ArrayList<Integer>();
         for(int i = 0; i < timeline.size(); i++){
-            yearList.add(i, timeline.get(i).yearnumber);
+            yearList.add(i, timeline.get(i).yearNumber);
         }
         Collections.sort(yearList);
         return yearList;
     }
     public void addYear(Year newYear){
-
+        if(timeline.contains(newYear)){
+            System.out.println("Year already Exists! Year not added");
+        }
+        else{
+            timeline.add(newYear);
+        }
     }
 }
--- Year.java
+++ Year.java
@@ -2,5 +2,20 @@ import java.util.ArrayList;
 
 public class Year extends Timeline{
     ArrayList<Game> gameList = new ArrayList<Game>();
-    int yearnumber;
+
+    public int getYearNumber() {
+        return yearNumber;
+    }
+
+    int yearNumber;
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj instanceof Year){
+            if(this.yearNumber == ((Year) obj).yearNumber){
+                return true;
+            }
+        }
+        return false;
+    }
 }
