3ba4cd5e34c4e2dd2bcc313390abdacfa2ca5caf
adrianxmp <adrian.ponce@ucalgary.ca>
2022-03-20 01:43:06 -0600

    Added getGamesWithATie method in the Output class

--- project/menu/Output.java
+++ project/menu/Output.java
@@ -1,5 +1,6 @@
 package project.menu;
 
+import project.data.Game;
 import project.data.Timeline;
 
 import java.util.ArrayList;
@@ -44,6 +45,7 @@ public class Output extends Menu{
     public static List<String> topShots() {
         ArrayList<String> timelineList = new ArrayList<>();
         ArrayList<String> shots = new ArrayList<>();
+        timelineList.add(String.valueOf(Timeline.getTimeline()));
         for (int i = 0; i < timelineList.size(); i++) {
             for (int j = 0; j < Timeline.getYear(i).getGameList().size(); j++) {
                 shots.add(String.valueOf(Timeline.getYear(i).getGameList().get(j).getHomeShots()));
@@ -58,6 +60,7 @@ public class Output extends Menu{
     public static List<String> topGoals(){
         ArrayList<String> timelineList = new ArrayList<>();
         ArrayList<String> goals = new ArrayList<>();
+        timelineList.add(String.valueOf(Timeline.getTimeline()));
         for (int i = 0; i < timelineList.size(); i++) {
             for (int j = 0; j < Timeline.getYear(i).getGameList().size(); j++) {
                 goals.add(String.valueOf(Timeline.getYear(i).getGameList().get(j).getHomeGoals()));
@@ -68,5 +71,21 @@ public class Output extends Menu{
         List<String> topFiveGoals = goals.subList(0, 5);
         return topFiveGoals;
     }
-
+    public static int getGamesWithATie(){
+        ArrayList<String> timelineList = new ArrayList<>();
+        ArrayList<String> gameOutcome = new ArrayList<>();
+        timelineList.add(String.valueOf(Timeline.getTimeline()));
+        int tieGameCounter = 0;
+        for (int i = 0; i < timelineList.size(); i++) {
+            for (int j = 0; j < Timeline.getYear(i).getGameList().size(); j++) {
+                gameOutcome.add(String.valueOf(Timeline.getYear(i).getGameList().get(j).getGameWinner()));
+            }
+        }
+        for (String s : gameOutcome) {
+            if (Objects.equals(s, Game.Winner.TIE)) {
+                tieGameCounter++;
+            }
+        }
+        return tieGameCounter;
+    }
 }
