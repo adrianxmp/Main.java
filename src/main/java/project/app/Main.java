c8be5064fe004a1b8fc4f6da9c52c898a66e2f71
Larryman555 <larryman555@gmail.com>
2022-04-08 00:47:26 -0600

    Basic GUI layout (no xml)

--- src/main/java/project/app/Main.java
+++ src/main/java/project/app/Main.java
@@ -8,11 +8,12 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+    static final String version = "1.0";
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
+        stage.setTitle("Premier League Tracker v" + version);
         stage.setScene(scene);
         stage.show();
     }
