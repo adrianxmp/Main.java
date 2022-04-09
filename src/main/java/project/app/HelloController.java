5833ac0d2a802131b67627fef37172493627b7b3
adrianxmp <adrian.ponce@ucalgary.ca>
2022-04-09 15:50:03 -0600

    Added addYear action and fx ids, Minor fxml fixes

--- src/main/java/project/app/HelloController.java
+++ src/main/java/project/app/HelloController.java
@@ -2,13 +2,51 @@ package project.app;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import project.data.Timeline;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField homeFoulsInput;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField homeGoalsInput;
+
+    @FXML
+    private TextField homeShotsInput;
+
+    @FXML
+    private TextField homeTeamNameInput;
+
+    @FXML
+    private TextField awayFoulsInput;
+
+    @FXML
+    private TextField awayGoalsInput;
+
+    @FXML
+    private TextField awayShotsInput;
+
+    @FXML
+    private TextField awayTeamNameInput;
+
+    @FXML
+    private TextField gameDateInput;
+
+    @FXML
+    private TextField yearNumberInput;
+
+    @FXML
+    void addYearAction(MouseEvent event) {
+        Timeline.addYear(Integer.parseInt(yearNumberInput.getText()));
     }
+
+    @FXML
+    void addGameAction(MouseEvent event) {
+
+    }
+
+
+
 }
