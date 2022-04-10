43aa59887c5a5a2026636bae19006c5d313246cb
Larryman555 <larryman555@gmail.com>
2022-04-09 22:16:29 -0600

    fx ids added, Initialize created

--- src/main/java/project/app/HelloController.java
+++ src/main/java/project/app/HelloController.java
@@ -2,40 +2,29 @@ package project.app;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import project.data.Timeline;
 
 public class HelloController {
-    @FXML
-    private TextField homeFoulsInput;
-
-    @FXML
-    private TextField homeGoalsInput;
-
-    @FXML
-    private TextField homeShotsInput;
-
-    @FXML
-    private TextField homeTeamNameInput;
-
-    @FXML
-    private TextField awayFoulsInput;
-
-    @FXML
-    private TextField awayGoalsInput;
-
-    @FXML
-    private TextField awayShotsInput;
-
-    @FXML
-    private TextField awayTeamNameInput;
-
-    @FXML
-    private TextField gameDateInput;
-
-    @FXML
-    private TextField yearNumberInput;
+    @FXML private TextField homeFoulsInput;
+    @FXML private TextField homeGoalsInput;
+    @FXML private TextField homeShotsInput;
+    @FXML private TextField homeTeamNameInput;
+    @FXML private TextField awayFoulsInput;
+    @FXML private TextField awayGoalsInput;
+    @FXML private TextField awayShotsInput;
+    @FXML private TextField awayTeamNameInput;
+    @FXML private TextField gameDateInput;
+    @FXML private TextField yearNumberInput;
+    @FXML private TextField generalHomeInput;
+    @FXML private TextField generalAwayInput;
+    @FXML private TextField generalDateInput;
+    @FXML private TextField generalYearInput;
+    @FXML private TextArea display;
+    @FXML private Label errorStatus;
+    @FXML private Label displayStatus;
 
     @FXML
     void addYearAction(MouseEvent event) {
@@ -46,7 +35,16 @@ public class HelloController {
     void addGameAction(MouseEvent event) {
 
     }
-
+    /**
+     * General GUI setup stuff
+     * @author Robert Engel, T03, April 9 2022
+     * (I'm sure Adrian will add stuff here too)
+     */
+    @FXML
+    public void initialize(){
+        displayStatus.setText("");
+        errorStatus.setText("");
+    }
 
 
 }
